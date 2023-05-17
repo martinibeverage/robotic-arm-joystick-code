0810df55d925a69bc834b784735b12cc89a64b3d
martinibeverage <122539603+martinibeverage@users.noreply.github.com>
2023-05-17 09:03:08 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,18 +3,18 @@ radio.setGroup(7)
 basic.showString("T")
 basic.forever(function () {
     if (joystickbit.getRockerValue(joystickbit.rockerType.X) < 10) {
-        radio.setGroup(4)
-    } else if (joystickbit.getRockerValue(joystickbit.rockerType.X) < 1010) {
-        radio.setGroup(3)
+        radio.sendNumber(4)
+    } else if (joystickbit.getRockerValue(joystickbit.rockerType.X) > 1010) {
+        radio.sendNumber(3)
     } else if (joystickbit.getRockerValue(joystickbit.rockerType.Y) < 10) {
-        radio.setGroup(2)
-    } else if (joystickbit.getRockerValue(joystickbit.rockerType.Y) < 1010) {
-        radio.setGroup(1)
+        radio.sendNumber(2)
+    } else if (joystickbit.getRockerValue(joystickbit.rockerType.Y) > 1010) {
+        radio.sendNumber(1)
     } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P12)) {
-        radio.setGroup(5)
+        radio.sendNumber(5)
     } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P13)) {
-        radio.setGroup(6)
+        radio.sendNumber(6)
     } else {
-        radio.setGroup(0)
+        radio.sendNumber(0)
     }
 })
